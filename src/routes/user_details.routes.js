fce77f7d6fc49e2e2ed2d7302f9b9cd5d05091a6
Goncalo Silva <95281891+GoncalofmSilva@users.noreply.github.com>
2025-07-01 10:55:55 +0100

    add authorize middleware

--- src/routes/user_details.routes.js
+++ src/routes/user_details.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { listUsersFilter} from "../controllers/user_details.controller.js";
+import authorize from "../middlewares/auth.middleware.js"
 
 const userDetailsRouter = Router()
 
-userDetailsRouter.post('/users', listUsersFilter)
-
+userDetailsRouter.post('/users', authorize, listUsersFilter)
 
 export default userDetailsRouter
